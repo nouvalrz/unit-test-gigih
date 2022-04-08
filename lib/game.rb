70d89d68907f8aeb0b27ddb3b2a1b0e10db418d7
Nouval Rizky <nouvalr@gmail.com>
2022-04-08 09:39:13 +0000

    hands-on complete

---
+++ lib/game.rb
@@ -0,0 +1,19 @@
+class Game
+  def initialize(secret_word)
+    @secret_word = secret_word
+  end
+
+  def match_word(guess_word)
+    result = "\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E6}"
+
+    guess_word.split('').each_with_index do |letter, index|
+      if @secret_word[index] == letter
+        result[index] = "\u{1F7E9}"
+      elsif @secret_word.include?(letter)
+        result[index] = "\u{1F7E8}"
+      end 
+    end
+    result
+  end
+
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,34 @@
+require 'game'
+
+describe 'Game' do
+  describe '#match_word' do
+    it 'should handle all incorrect letters well' do
+      game = Game.new('DRINK')
+      guess_word = 'HELLO'
+
+      result = game.match_word(guess_word)
+
+      expect(result).to eq("\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E6}")
+    end
+
+    it 'should handle a wrongly positioned letter well' do
+      game = Game.new('DRINK')
+      guess_word = 'CLEAN'
+
+      result = game.match_word(guess_word)
+
+      expect(result).to eq("\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E6}\u{1F7E8}")
+    end
+
+    it 'should handle a correctly positioned letter well' do
+      game = Game.new('DRINK')
+      guess_word = 'ALIVE'
+    
+      result = game.match_word(guess_word)
+
+      expect(result).to eq("\u{1F7E6}\u{1F7E6}\u{1F7E9}\u{1F7E6}\u{1F7E6}")
+    end
+
+  
+  end
+end
